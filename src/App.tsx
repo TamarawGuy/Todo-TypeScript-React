f857b76b6f3d5d5a9a1148a4ddd5b3dcf8a70281
Viktor <viktor.m.rumenov@gmail.com>
2022-04-26 16:51:30 +0200

    Added toggleTodo functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoListItem from "./components/TodoListItem";
 
 const initialTodos = [
@@ -13,10 +13,22 @@ const initialTodos = [
 ];
 
 const App = () => {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map((todo) => {
+      if (selectedTodo === todo) {
+        return { ...todo, complete: !todo.complete };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
   return (
     <div>
-      <TodoListItem todo={initialTodos[0]} />
-      <TodoListItem todo={initialTodos[1]} />
+      <TodoListItem todo={todos[0]} toggleTodo={toggleTodo} />
+      <TodoListItem todo={todos[1]} toggleTodo={toggleTodo} />
     </div>
   );
 };
--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -2,15 +2,21 @@ import React from "react";
 
 interface Props {
   todo: Todo;
+  toggleTodo: ToggleTodo;
 }
 
-const TodoListItem: React.FC<Props> = ({ todo }) => {
+const TodoListItem: React.FC<Props> = ({ todo, toggleTodo }) => {
   return (
     <li>
       <label
         style={{ textDecoration: todo.complete ? "line-through" : undefined }}
       >
-        <input type="checkbox" checked={todo.complete} /> {todo.text}
+        <input
+          type="checkbox"
+          checked={todo.complete}
+          onClick={() => toggleTodo(todo)}
+        />{" "}
+        {todo.text}
       </label>
     </li>
   );
